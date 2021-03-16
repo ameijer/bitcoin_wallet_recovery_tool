9b7286fadf58af5129752c21b2d83113f9324c99
alex <alex.t.meijer@gmail.com>
2021-03-15 21:04:51 -0400

    begin sketching out tests for main class

---
+++ src/test/java/com/ameijer/bruteforce/TestMain.java
@@ -0,0 +1,20 @@
+package com.ameijer.bruteforce;
+
+import org.junit.jupiter.api.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.UUID;
+import java.io.IOException;
+import java.util.Random;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+
+    @Test
+    public void testBytesToHex() {
+    	// TODO
+    }
+    
+
+
+}
